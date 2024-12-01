1f123b4db9ee9c057558c750365df68f2bf6a1e6
mosman <mariamhiosman@gmail.com>
2024-12-01 01:23:03 +0000

    Added stock price class and mock data for frontend

--- src/main/java/com/marketdata/MarketSimulatorBackend/controller/StockController.java
+++ src/main/java/com/marketdata/MarketSimulatorBackend/controller/StockController.java
@@ -1,2 +1,27 @@
-package com.marketdata.MarketSimulatorBackend.controller;public class StockController {
+package com.marketdata.MarketSimulatorBackend.controller;
+
+import com.marketdata.MarketSimulatorBackend.model.StockPrice;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * StockController is a REST controller that provides endpoints
+ * to serve stock price data.
+ */
+@RestController
+@RequestMapping("/api") // Base URL for all endpoints in this controller
+public class StockController {
+    @GetMapping("/stocks")
+    public List<StockPrice> getMockStockPrices() {
+        return Arrays.asList(
+                new StockPrice("Company A", 150, "2024-11-01T10:00:00"),
+                new StockPrice("Company A", 180, "2024-11-01T11:00:00"),
+                new StockPrice("Company B", 200, "2024-11-01T10:00:00"),
+                new StockPrice("Company C", 250, "2024-11-01T11:00:00")
+        );
+    }
 }
---
+++ src/test/java/com/marketdata/MarketSimulatorBackend/MarketSimulatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketdata.MarketSimulatorBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketSimulatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
